3b37b00d89b5956dafaaa2595a6c5cd6750c72cd
AminEtemadifar <aminetemadifar.w@gmail.com>
2025-06-02 23:35:46 +0330

    upgrade

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,29 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BranchController;
+use App\Http\Controllers\LessonController;
+use App\Http\Controllers\LessonTypeController;
+use App\Http\Controllers\RankController;
 use Illuminate\Support\Facades\Route;
 
+// Test connection route
 Route::get('/', function () {
     return response()->json('connection successful');
 });
-Route::post('/login', [App\Http\Controllers\AuthController::class, 'store']);
-Route::middleware(['auth:student'])->get('/logout', [App\Http\Controllers\AuthController::class, 'destroy']);
+
+// Auth routes
+Route::post('/login', [AuthController::class, 'store']);
+Route::middleware(['auth:student'])->get('/logout', [AuthController::class, 'destroy']);
+
+// Branch routes
+Route::apiResource('branches', BranchController::class)->only('index');
+
+// Lesson routes
+Route::apiResource('lessons', LessonController::class)->only('index');
+
+// Lesson Type routes
+Route::apiResource('lesson-types', LessonTypeController::class)->only('index');
+
+// Rank routes
+Route::apiResource('ranks', RankController::class)->only('index');

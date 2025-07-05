f86bb152a063dc59980b9f1c926c5b62774b56f4
AminEtemadifar <aminetemadifar.w@gmail.com>
2025-07-05 20:43:10 +0330

    message

--- app/Http/Resources/MessageResource.php
+++ app/Http/Resources/MessageResource.php
@@ -14,6 +14,7 @@ class MessageResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        dd($this);
         return [
             'id' => $this->id,
             'message' => $this->message,
